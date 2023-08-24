2c6d4d49f1e83794cb3e163c2981030edebfc1b0
vipul200 <78165670+vipul200@users.noreply.github.com>
2023-08-24 16:43:22 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
 print('hello world')
+l1 = [4,2,45,6,7,3,6,8,0,1,6,7,9,7,5]
+
+l2=sorted(l1) # sorted function returns new list of any iterable obj in sorted keys
+
+print('sorted list : ',l2)
+print('original list : ',l1)
+
+l1.sort()
+print('original list : ',l1)
+
+# sorting in descending order
+
+l3 = sorted(l1,reverse=True)
+
+print('sorted list in desc : ',l3)
+
+t1 = (4,2,45,6,7,3,6,8,0,1,6,7,9,7,5)
+
